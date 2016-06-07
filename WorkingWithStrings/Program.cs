951b64344f7239178f2ed99296074f155713a1c7
Michael <Michael@DESKY>
2016-06-06 20:12:33 -0400

    commit

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -10,6 +10,50 @@ namespace WorkingWithStrings
     {
         static void Main(string[] args)
         {
+            //string myString = "";
+            //string myString = "My \"so-called\" life";
+            //string myString = "What if I need a\nnew line?";
+            //string myString = "Go to your C:\\ drive";
+            //string myString = @"Go to your C:\ drive";
+
+            //string myString = String.Format("{1} = {0}", "First", "Second");
+
+            //string myString = string.Format("{0:}", 123.45);
+            //string myString = string.Format("{0:N}", 1234568790);
+            //string myString = string.Format("Percentage: {0:P}", .123);
+            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);
+
+            //string myString = " That summer we took threes across the board   ";
+
+            //myString = myString.Substring(6);
+            //myString = myString.ToUpper();
+            //myString = myString.Replace(" ", "--");
+            //myString = myString.Remove(6, 14);
+
+            /*myString = String.Format("Length before: {0} -- Length after: {1}",
+                myString.Length,
+                myString.Trim().Length);
+            */
+
+            /*
+            string myString = "";
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            }
+            */
+
+            /*
+            StringBuilder myString = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+            */
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
         }
     }
 }
